b088b5fde2d6b7543bdbaf05a2912fce72dc3c97
daveying <daveying@163.com>
2018-04-04 21:14:11 +0800

    first example works

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// src/index.ts
+
+import Vue from "vue";
+
+let v = new Vue({
+    el: "#app",
+    template: `
+    <div>
+        <div>Hello {{name}}!</div>
+        Name: <input v-model="name" type="text">
+    </div>
+    `,
+    data: {
+        name: "World"
+    }
+});
