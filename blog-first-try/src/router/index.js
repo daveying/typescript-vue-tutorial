6fea341425741d3f0e7193318733fc8606c6f0c6
daveying <daveying@163.com>
2018-04-07 22:16:29 +0800

    add contact component

--- blog-first-try/src/router/index.js
+++ blog-first-try/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import AppHome from '@/components/AppHome'
 import AppLogin from '@/components/AppLogin'
+import AppContact from '@/components/AppContact'
 
 Vue.use(Router)
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/login',
       name: 'AppLogin',
       component: AppLogin
+    },
+    {
+      path: '/contact',
+      name: 'AppContact',
+      component: AppContact
     }
   ]
 })
