9567e2e2c7d4d8517e458abe2178bea4147c5799
daveying <daveying@163.com>
2018-04-12 21:44:20 +0800

    add intro-vuetify

---
+++ blog-second-try/src/components/MainLayout.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-navigation-drawer
+        fixed
+        :mini-variant="miniVariant"
+        :clipped="clipped"
+        v-model="drawer"
+        app
+      >
+        <v-list>
+          <v-list-tile :value="true" v-for="(item, i) in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon light v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar fixed app :clipped-left="clipped">
+        <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-btn 
+          icon
+          @click.stop="miniVariant = !miniVariant"
+        >
+          <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          @click.stop="clipped = !clipped"
+        >
+          <v-icon>web</v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          @click.native.stop="fixed = !fixed"
+        >
+          <v-icon>remove</v-icon>
+        </v-btn>
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+          icon
+          @click.native.stop="rightDrawer = !rightDrawer"
+        >
+          <v-icon>menu</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+      <v-navigation-drawer
+        fixed
+        temporary
+        :right="right"
+        v-model="rightDrawer"
+        app
+      >
+        <v-list>
+          <v-list-tile @click.native="right = !right">
+            <v-list-tile-action>
+              <v-icon light>compare_arrows</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        miniVariant: true,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+    }
+  }
+}
+</script>
