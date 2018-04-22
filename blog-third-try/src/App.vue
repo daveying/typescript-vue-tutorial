51d49c4f80684f5c64e3487e8907a02a9c2bcfd6
daveying <daveying@163.com>
2018-04-22 20:00:22 +0800

    third try

---
+++ blog-third-try/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <!-- <v-toolbar-side-icon 
+        @click="sideNav = !sideNav"
+        class="hidden-sm-and-up">
+      </v-toolbar-side-icon> -->
+      <v-toolbar-items>
+        <v-layout justify-space-around align-center>
+          <v-avatar @click="sideNav = !sideNav" style="cursor: pointer" color="blue" size="35">
+            <img src="/static/photo.jpg" alt="logo">
+          </v-avatar>
+        </v-layout>
+      </v-toolbar-items>
+      <v-toolbar-title>Xingpeng Da</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer 
+      v-model="sideNav"
+      >
+      <v-list>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false
+    }
+  },
+  name: 'App'
+}
+</script>
