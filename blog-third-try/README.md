6a6ee64815fd73ff79763cb8e7a6bc75734507c3
daveying <daveying@163.com>
2018-04-23 22:26:54 +0800

    add carousel

--- blog-third-try/README.md
+++ blog-third-try/README.md
@@ -19,3 +19,5 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+[Youtube tutorial](https://www.youtube.com/watch?v=AesqUS2udb4&index=8&list=PL55RiY5tL51qxUbODJG9cgrsVd7ZHbPrt)
--- blog-third-try/src/components/Home.vue
+++ blog-third-try/src/components/Home.vue
@@ -1,5 +1,62 @@
 <template>
-  <div>
-      The home page
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large to="/meetups" class="primary">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large to="/meetup/new" class="primary">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout xs12 class="mt-2">
+      <v-carousel>
+        <v-carousel-item 
+          v-for="meetup in meetups" 
+          :src="meetup.imageUrl" 
+          :key="meetup.id">
+          <v-flex xs12 class="text-xs-center">
+            <div class="title">
+              {{meetup.title}}
+            </div>
+          </v-flex>
+        </v-carousel-item>
+      </v-carousel>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awsome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+          id: 'adsdweadwed',
+          title: 'Meetup in New York'
+        },
+        { imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/0d/f4/db/9e/paris-in-one-day-sightseeing.jpg',
+          id: 'sdafwewswda',
+          title: 'Meetup in Paris'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.5);
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
+
