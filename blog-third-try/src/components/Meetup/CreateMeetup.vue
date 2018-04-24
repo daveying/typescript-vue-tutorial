dcc0d993a11d63fbf06becfa0962ea88224f49d7
daveying <daveying@163.com>
2018-04-24 23:24:23 +0800

    add create meetup

--- blog-third-try/src/components/Meetup/CreateMeetup.vue
+++ blog-third-try/src/components/Meetup/CreateMeetup.vue
@@ -7,7 +7,7 @@
     </v-layout>
     <v-layout row>
       <v-flex xs12>
-        <form>
+        <form @submit.prevent="onCreateMeetup">
           <v-layout>
             <v-flex xs12 sm6 offset-sm2>
               <v-text-field
@@ -18,6 +18,55 @@
                 id="title" 
                 required
                 ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs12 sm6 offset-sm2>
+              <v-text-field
+                name="location"
+                label="Location"
+                v-model="location"
+                :rules="locationRules"
+                id="location" 
+                required
+                ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs12 sm6 offset-sm2>
+              <v-text-field
+                name="imageUrl"
+                label="Image URL"
+                v-model="imageUrl"
+                id="imageUrl" 
+                required
+                ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs12 sm6 offset-sm2>
+              <img :src="imageUrl" height="250px" alt="uploaded file">
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs12 sm6 offset-sm2>
+              <v-text-field
+                name="description"
+                label="Description"
+                v-model="description"
+                id="description" 
+                required
+                multi-line
+                rows=3
+                ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm2>
+              <v-btn 
+                class="primary" 
+                :disabled="!formIsValid"
+                type="submit">Create Meetup</v-btn>
             </v-flex>
           </v-layout>
         </form>
@@ -31,10 +80,38 @@ export default {
   data () {
     return {
       title: '',
+      location: '',
+      imageUrl: '',
+      description: '',
       nameRules: [
         v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters'
+        v => v.length <= 30 || 'Name must be less than 30 characters'
+      ],
+      locationRules: [
+        v => !!v || 'Location is required',
+        v => v.length <= 60 || 'Location must be less than 60 characters'
       ]
+    }
+  },
+  computed: {
+    formIsValid () {
+      return this.title !== '' && this.location !== '' && this.imageUrl !== '' && this.description !== ''
+    }
+  },
+  methods: {
+    onCreateMeetup () {
+      if (!this.formIsValid) {
+        return
+      }
+      const meetupData = {
+        title: this.title,
+        location: this.location,
+        imageUrl: this.imageUrl,
+        description: this.description,
+        date: new Date()
+      }
+      this.$store.dispatch('createMeetup', meetupData)
+      this.$router.push('/meetups')
     }
   }
 }
--- blog-third-try/src/store/index.js
+++ blog-third-try/src/store/index.js
@@ -8,24 +8,41 @@ export const store = new Vuex.Store({
     loadedMeetups: [
       {
         imageUrl: 'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
-        id: 'adsdweadwed',
+        id: '1',
         title: 'Meetup in New York',
         date: '2017-07-17'
       },
       {
         imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/0d/f4/db/9e/paris-in-one-day-sightseeing.jpg',
-        id: 'sdafwewswda',
+        id: '2',
         title: 'Meetup in Paris',
         date: '2017-07-19'
       }
     ],
     user: {
       id: 'sdadewws',
-      registeredMeetups: ['adsdweadwed']
+      registeredMeetups: ['1']
     }
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    }
+  },
+  actions: {
+    createMeetup ({commit}, payload) {
+      const meetup = {
+        title: payload.title,
+        loacation: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: Math.floor(Math.random() * 1000000) + ''
+      }
+      // Reach out to firebase and store it and get back id
+      commit('createMeetup', meetup)
+    }
+  },
   getters: {
     loadedMeetups (state) {
       return state.loadedMeetups.sort((meetupA, meetupB) => {
